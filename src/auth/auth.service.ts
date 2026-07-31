6b3d882b97f7130f594125c5775e56704b525f36
noyandey88 <coder.noyan@gmail.com>
2026-07-31 18:33:06 +0600

    feat: added jwt access token functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, Logger } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { RegisterDto, LoginDto } from './dto/registerUser.dto';
 import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
   async registerUser(registerUserDto: RegisterDto) {
     Logger.log(registerUserDto);
 
@@ -17,6 +21,18 @@ export class AuthService {
   async loginUser(loginDto: LoginDto) {
     Logger.log(loginDto);
 
-    return this.userService.findUser(loginDto);
+    const user = await this.userService.findUser(loginDto);
+
+    const payload = { sub: user.id };
+    const token = await this.jwtService.signAsync(payload);
+
+    const expiresIn = 5 * 60; // 300 ms
+
+    return {
+      accessToken: token,
+      expiresIn,
+      expiresAt: Math.floor(Date.now() / 1000) + expiresIn,
+      user: user,
+    };
   }
 }
